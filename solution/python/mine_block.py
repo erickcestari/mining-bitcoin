95d9bda6b55919814a6bf53d94faef7339311dca
Erick Cestari <erickcestari03@gmail.com>
2025-02-13 12:41:31 -0300

    remove python solution

--- solution/python/mine_block.py
+++
@@ -1,7 +0,0 @@
-
-def mine_block():
-    print("Hello, world!")
-
-
-if __name__ == "__main__":
-    mine_block()
--- solution/run_miner.sh
+++
@@ -1,5 +0,0 @@
-# PYTHON
-#python ./solution/python/mine_block.py
-
-# RUST
-cargo run --manifest-path ./solution/rust/Cargo.toml -p mine_block
